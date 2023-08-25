d6687d988b8cabc38536d7c0c12e27d0e93d1ba6
nusrat-j <86145698+nusrat-j@users.noreply.github.com>
2023-08-25 15:05:43 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/* *Statement usig
+*Heading tag
+*br tag */
+
+	echo "<h1> Hello World! </h1><br>";
+	echo "<h3>Myself Akila Jahan, from Batch no-2301.</h3>";
+	echo "Doing My Industrial attatchment on Web Development. Thats my 1st homework at 24th August.<br><br>";
+
+//Number
+	$a = 45;
+	$b = 85;
+	echo $a + $b;
+
+
+// Variable declaration
+
+	$user_name = "<h4>AKILA JAHAN</h4>";
+	$institute = "<h4>Chittagong Mohila Polytechnic Institute</h4>";
+
+	echo $user_name,$institute;
+
+// concatenation
+
+		$name = "Akila";
+		$city = "Dhaka";
+
+	echo $name.' '.'went to'.' '.$city.' '.'on vacation<br><br>';
+	
+	
+
+// Contant declaration
+
+	define("id", "01");
+	echo id;
+	define("id" , "01")
+	
+?>
